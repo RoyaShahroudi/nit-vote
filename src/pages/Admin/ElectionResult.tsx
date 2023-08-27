fae8ac8ce4f1f51c4f22abfa36607965f698fd3e
r.shahroudi <roya.shahroudi20@gmail.com>
2023-08-27 21:11:21 +0330

    feat: Implement election result page

--- src/pages/Admin/ElectionResult.tsx
+++ src/pages/Admin/ElectionResult.tsx
@@ -1,45 +1,65 @@
-import React, {FC, SetStateAction, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import Header from "../../components/Navbar";
-import List from "../../components/List";
-import login from "./Login";
+import {useParams} from "react-router-dom";
+import {getTokens} from "../../utils";
+import FormAction from "../../components/FormAction";
 
-const items = [
-    {
-        id: 0,
-        label: "Candidate 1",
-        voteCount: 3
-    }
-    ,
-    {
-        id: 1,
-        label: "Candidate 2",
-        voteCount: 5
-    },
-    {
-        id: 2,
-        label: "Candidate 3",
-        voteCount: 1
-    }
-]
+interface ElectionState {
+    electionName: string;
+    candidateResults?: {candidate: {
+        id: number ,
+        name: string,
+        info: string
+        },
+    voteCount: number ,
+    }[]
+}
 
 const ElectionResult: FC<{}> = () => {
+    const [election, setElection] = useState<ElectionState>();
+    const params = useParams();
 
+    useEffect(() => {
+        getElectionResult()
+    }, []);
+
+    const getElectionResult = () => {
+        const tokens = getTokens();
+        fetch(`${import.meta.env.VITE_API_BASE_URL}/v1/election/result/${params.id}`, {
+            method: "GET",
+            headers: {Authorization: `Bearer ${tokens?.token}`}
+        }).then(async (response: any) => {
+                if (response.ok) {
+                    const res = await response.json();
+                    console.log("getElectionResult: ", res)
+                    setElection(res);
+                } else {
+                    throw new Error('Error: ' + response.status);
+                }
+            }
+        ).catch(error => {
+            console.log("error: ", error)
+        })
+    }
 
     return (
         <>
             <Header admin text="نتایج"/>
             <div className="container px-3 ">
                 <div className="mb-5">
-                    voting subject
+                    {election && election.electionName}
                 </div>
-                {items && items.length ? (
-                    items.map(item => {
+                {election && election.candidateResults && election.candidateResults.length ? (
+                    election.candidateResults.map(item => {
                         return (
                             <div
                                 className="flex justify-between w-96 px-2 py-2 mb-3 bg-blue-600 text-white">
                                 <div>
-                                    <span>{item.id + 1}. </span>
-                                    {item.label ? (<span>{item.label}</span>) : null}
+                                    {item.candidate && item.candidate.name ? (
+                                        <span>{item.candidate.name}</span>) : null}
+                                    -
+                                    {item.candidate && item.candidate.info ? (
+                                        <span>{item.candidate.info}</span>) : null}
                                 </div>
                                 <div>
                                     {item.voteCount} رای
@@ -49,6 +69,7 @@ const ElectionResult: FC<{}> = () => {
                     })
                 ) : null}
             </div>
+            <FormAction handleSubmit={getElectionResult} text="بروزرسانی" />
         </>
     )
 }
